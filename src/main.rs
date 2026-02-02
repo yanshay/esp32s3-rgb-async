98ec1e562c4ebc15d365fdf48bdba84c0b34424b
yanshay <yanshay>
2026-02-02 21:48:10 +0200

    branch: added the rgb sync code into a task, single task work, not single task doen't work well

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@
 #![deny(clippy::large_stack_frames)]
 #![feature(box_as_ptr)]
 
+// SEE my work on this in ////////////////////////////////////////////////////////////////////
+// https://github.com/esp-rs/esp-hal/issues/2884
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 use esp_hal::clock::CpuClock;
 use esp_hal::dma::{CHUNK_SIZE, DmaDescriptor, DmaTxBuf};
 use esp_hal::gpio::{Level, Output, OutputConfig};
@@ -69,9 +73,6 @@ const FRAME_BYTES: usize = (LCD_H_RES as usize * LCD_V_RES as usize) * 2;
     clippy::large_stack_frames,
     reason = "it's not unusual to allocate larger buffers etc. in main"
 )]
-
-
-
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
     esp_println::logger::init_logger_from_env();
@@ -108,29 +109,307 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
-    // Done with esp-hal initialization /////////////////////////////////////////////////////////////
+    Timer::after_millis(10).await;
 
-    // Prepare shared memory for drive_display and app tasks ////////////////////////////////////////
+    // turn on backlight
+    let _ = Output::new(peripherals.GPIO2, Level::High, OutputConfig::default());
+
+    let delay = esp_hal::delay::Delay::new();
+
+    let tx_channel = peripherals.DMA_CH2;
+    let lcd_cam = LcdCam::new(peripherals.LCD_CAM);
+
+    info!("Initialising Display");
+
+    // let mut vsync_pin = peripherals.GPIO41;
+    // let vsync_must_be_high_during_setup =
+    //     Output::new(vsync_pin.reborrow(), Level::High, OutputConfig::default());
+    // drop(vsync_must_be_high_during_setup);
+
+    // // ST7262 IPS LCD 800x480
+    //  Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
+    //    bus,
+    //    800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
+    //    480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
+    //    1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+
+    let config = esp_hal::lcd_cam::lcd::dpi::Config::default()
+        .with_clock_mode(esp_hal::lcd_cam::lcd::ClockMode {
+            polarity: esp_hal::lcd_cam::lcd::Polarity::IdleLow,
+            phase: esp_hal::lcd_cam::lcd::Phase::ShiftLow,
+        })
+        .with_frequency(Rate::from_mhz(16))
+        .with_format(esp_hal::lcd_cam::lcd::dpi::Format {
+            enable_2byte_mode: true,
+            ..Default::default()
+        })
+        .with_timing(esp_hal::lcd_cam::lcd::dpi::FrameTiming {
+            horizontal_active_width: 800,
+            horizontal_total_width: 840,
+            horizontal_blank_front_porch: 10,
+
+            vertical_active_height: 480,
+            vertical_total_height: 504,
+            vertical_blank_front_porch: 12,
+
+            hsync_width: 4,
+            vsync_width: 4,
+
+            hsync_position: 10,
+        })
+        .with_vsync_idle_level(Level::High)
+        .with_hsync_idle_level(Level::High)
+        .with_de_idle_level(Level::Low)
+        .with_disable_black_region(false);
+
+    // Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+    //     GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
+    //     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+    //     45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
+    //     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
+    //     8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
+    // );
+
+    let mut dpi = esp_hal::lcd_cam::lcd::dpi::Dpi::new(lcd_cam.lcd, tx_channel, config)
+        .unwrap()
+        .with_vsync(peripherals.GPIO41) // 41
+        .with_hsync(peripherals.GPIO39)
+        .with_de(peripherals.GPIO40)
+        .with_pclk(peripherals.GPIO42)
+        // Blue
+        .with_data0(peripherals.GPIO8)
+        .with_data1(peripherals.GPIO3)
+        .with_data2(peripherals.GPIO46)
+        .with_data3(peripherals.GPIO9)
+        .with_data4(peripherals.GPIO1)
+        // Green
+        .with_data5(peripherals.GPIO5)
+        .with_data6(peripherals.GPIO6)
+        .with_data7(peripherals.GPIO7)
+        .with_data8(peripherals.GPIO15)
+        .with_data9(peripherals.GPIO16)
+        .with_data10(peripherals.GPIO4)
+        // Red
+        .with_data11(peripherals.GPIO45)
+        .with_data12(peripherals.GPIO48)
+        .with_data13(peripherals.GPIO47)
+        .with_data14(peripherals.GPIO21)
+        .with_data15(peripherals.GPIO14);
+
+    ///////////////////////////////////////////////////////////////////
+    // PSRAM rendering code
+    ///////////////////////////////////////////////////////////////////
+
+    const FRAME_PIXELS: usize = (LCD_H_RES as usize) * (LCD_V_RES as usize);
+    const FRAME_BYTES: usize = FRAME_PIXELS * 2;
 
     // Allocate a PSRAM-backed DMA buffer for the frame
-    let original_buf_box: Box<[u8; FRAME_BYTES]> = Box::new([0; FRAME_BYTES]);
-    let ptr = Box::into_raw(original_buf_box);
-    let buf_box: Box<[u8; FRAME_BYTES]> = unsafe { Box::from_raw(ptr) };
-    let buf_box2: Box<[u8; FRAME_BYTES]> = unsafe { Box::from_raw(ptr) };
+    let mut buf_box: Box<[u8; FRAME_BYTES]> = Box::new([0; FRAME_BYTES]);
 
-    // core0_spawner.spawn(drive_display(buf_box)).ok();
-    spawner.spawn(drive_display(buf_box)).ok();
-    spawner.spawn(app(buf_box2)).ok();
+    let ptr = Box::as_mut_ptr(&mut buf_box);
+    let buf_box2: &mut [u8; FRAME_BYTES] = unsafe { &mut *ptr };
+    let buf_box3: Box<[u8; FRAME_BYTES]> = unsafe { Box::from_raw(ptr) };
 
-    // Done with UI stuff initialization /////////////////////////////////////////////////////////////
-
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+    let single_task = false;
+    if !single_task {
+        spawner.spawn(fill_data_for_previously_sync_code(buf_box3)).ok();
+        Timer::after_secs(1).await;
     }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
+    #[allow(clippy::manual_div_ceil)]
+    const NUM_DMA_DESC: usize = (FRAME_BYTES + CHUNK_SIZE - 1) / CHUNK_SIZE;
+    #[unsafe(link_section = ".dma")]
+    static mut TX_DESCRIPTORS: [DmaDescriptor; NUM_DMA_DESC] = [DmaDescriptor::EMPTY; NUM_DMA_DESC];
+
+    let psram_buf: &'static mut [u8] = Box::leak(buf_box);
+    #[allow(static_mut_refs)]
+    let mut dma_tx: DmaTxBuf = unsafe { DmaTxBuf::new(&mut TX_DESCRIPTORS, psram_buf).unwrap() };
+
+    unsafe {
+        TX_DESCRIPTORS[NUM_DMA_DESC - 1].next = &mut TX_DESCRIPTORS[0];
+        TX_DESCRIPTORS[NUM_DMA_DESC - 1].set_suc_eof(true);
+    }
+
+    // Clear screen to blue for sanity check
+    let [red_lo, red_hi] = Rgb565::RED.into_storage().to_le_bytes();
+    let [blue_lo, blue_hi] = Rgb565::BLUE.into_storage().to_le_bytes();
+    let dst = dma_tx.as_mut_slice();
+    {
+        for pixel in 0..FRAME_PIXELS {
+            dst[2 * pixel] = red_lo;
+            dst[2 * pixel + 1] = red_hi;
+        }
+    }
+
+    // for x in 0.. 480 {
+    //     buf_box2[(x* LCD_H_RES as usize + x)*2] = red_lo;
+    //     buf_box2[x*LCD_H_RES as usize + 1] = red_hi;
+    // }
+
+    // fn rgb(r: u16, g: u16, b: u16) -> u16 {
+    //     (r << 11) | (g << 5) | b
+    // }
+    //
+    // const MAX_RED: u16 = (1 << 5) - 1;
+    // const MAX_GREEN: u16 = (1 << 6) - 1;
+    // const MAX_BLUE: u16 = (1 << 5) - 1;
+    // Initial flush of the blue screen
+    let mut x = 0;
+    let mut y = 0;
+    let mut dx: isize = 1;
+    let mut dy: isize = 1;
+    let xfer = dpi.send(true, dma_tx).ok();
+
+    // info!("{i}");
+    let _start = esp_hal::time::Instant::now();
+    info!(">>>> going into send");
+    info!(">>>> send returned Ok");
+
+    if single_task {
+        loop {
+            // info!("in loop");
+            // buf_box2[(y * LCD_H_RES as usize + x) * 2] = blue_lo;
+            // buf_box2[(y * LCD_H_RES as usize + x) * 2 + 1] = blue_hi;
+            let idx = (y * LCD_H_RES as usize + x) * 2;
+            buf_box2[idx] = blue_lo;
+            buf_box2[idx + 1] = blue_hi;
+
+            // cache_writeback(unsafe { buf_box2.as_ptr().add(idx) }, 2);
+            unsafe {
+                cache_writeback_addr(buf_box2.as_ptr().add(idx) as u32, 2);
+            }
+
+            if x >= 799 {
+                dx = -1;
+            };
+            if x == 0 {
+                dx = 1;
+            };
+            if y >= 479 {
+                dy = -1;
+            };
+            if y == 0 {
+                dy = 1;
+            };
+            x = (x as isize + dx) as usize;
+            y = (y as isize + dy) as usize;
+
+            // let (_res, dpi2, tx2) = xfer.wait();
+            // info!("{x}, {y}");
+            // delay.delay_millis(10);
+            Timer::after_millis(10).await;
+        }
+    } else {
+        loop {
+            Timer::after_millis(1000).await;
+            info!("main task doing nothing");
+        }
+    }
 }
+
+#[embassy_executor::task]
+pub async fn fill_data_for_previously_sync_code(mut buf_box2: Box<[u8; FRAME_BYTES]>) {
+    Timer::after_secs(2).await; // part of fix
+    let [red_lo, red_hi] = Rgb565::RED.into_storage().to_le_bytes();
+    let [blue_lo, blue_hi] = Rgb565::BLUE.into_storage().to_le_bytes();
+    let mut x = 0;
+    let mut y = 0;
+    let mut dx: isize = 1;
+    let mut dy: isize = 1;
+    loop {
+        // info!("in loop");
+        // buf_box2[(y * LCD_H_RES as usize + x) * 2] = blue_lo;
+        // buf_box2[(y * LCD_H_RES as usize + x) * 2 + 1] = blue_hi;
+        let idx = (y * LCD_H_RES as usize + x) * 2;
+        buf_box2[idx] = blue_lo;
+        buf_box2[idx + 1] = blue_hi;
+
+        // cache_writeback(unsafe { buf_box2.as_ptr().add(idx) }, 2);
+        unsafe {
+            cache_writeback_addr(buf_box2.as_ptr().add(idx) as u32, 2);
+        }
+
+        if x >= 799 {
+            dx = -1;
+        };
+        if x == 0 {
+            dx = 1;
+        };
+        if y >= 479 {
+            dy = -1;
+        };
+        if y == 0 {
+            dy = 1;
+        };
+        x = (x as isize + dx) as usize;
+        y = (y as isize + dy) as usize;
+
+        // let (_res, dpi2, tx2) = xfer.wait();
+        // info!("{x}, {y}");
+        // delay.delay_millis(10);
+        Timer::after_millis(10).await;
+    }
+}
+
+// #[esp_rtos::main]
+// async fn main(spawner: Spawner) -> ! {
+//     esp_println::logger::init_logger_from_env();
+//     let psram_config = esp_hal::psram::PsramConfig {
+//         ram_frequency: esp_hal::psram::SpiRamFreq::Freq80m,
+//         ..Default::default()
+//     };
+//
+//     let config = esp_hal::Config::default()
+//         .with_cpu_clock(CpuClock::max())
+//         .with_psram(psram_config);
+//     let peripherals = esp_hal::init(config);
+//
+//     let (start, size) = esp_hal::psram::psram_raw_parts(&peripherals.PSRAM);
+//
+//     // IMPORTANT: PSRAM need to be initialized first, so 'Normal' allocations will use the region
+//     info!("Before init_psram_heap");
+//     init_psram_heap(start, size);
+//     esp_alloc::heap_allocator!(size: 300 * 1024);
+//     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
+//
+//     let timg0 = TimerGroup::new(peripherals.TIMG0);
+//     esp_rtos::start(timg0.timer0);
+//
+//     // let sw_int =
+//     //     esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+//     //
+//     // static EXECUTOR_CORE_0: static_cell::StaticCell<esp_rtos::embassy::InterruptExecutor<2>> =
+//     //     static_cell::StaticCell::new();
+//     // let executor_core0 = esp_rtos::embassy::InterruptExecutor::new(sw_int.software_interrupt2);
+//     // let executor_core0 = EXECUTOR_CORE_0.init(executor_core0);
+//     //
+//     // let core0_spawner = executor_core0.start(esp_hal::interrupt::Priority::Priority1);
+//
+//     info!("Embassy initialized!");
+//
+//     // Done with esp-hal initialization /////////////////////////////////////////////////////////////
+//
+//     // Prepare shared memory for drive_display and app tasks ////////////////////////////////////////
+//
+//     // Allocate a PSRAM-backed DMA buffer for the frame
+//     let original_buf_box: Box<[u8; FRAME_BYTES]> = Box::new([0; FRAME_BYTES]);
+//     let ptr = Box::into_raw(original_buf_box);
+//     let buf_box: Box<[u8; FRAME_BYTES]> = unsafe { Box::from_raw(ptr) };
+//     let buf_box2: Box<[u8; FRAME_BYTES]> = unsafe { Box::from_raw(ptr) };
+//
+//     // core0_spawner.spawn(drive_display(buf_box)).ok();
+//     spawner.spawn(drive_display(buf_box)).ok();
+//     spawner.spawn(app(buf_box2)).ok();
+//
+//     // Done with UI stuff initialization /////////////////////////////////////////////////////////////
+//
+//     loop {
+//         info!("Hello world!");
+//         Timer::after(Duration::from_secs(1)).await;
+//     }
+//
+//     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
+// }
 
 #[embassy_executor::task]
 pub async fn drive_display(buf_box: Box<[u8; FRAME_BYTES]>) {
@@ -227,8 +506,8 @@ pub async fn drive_display(buf_box: Box<[u8; FRAME_BYTES]>) {
     #[allow(static_mut_refs)]
     let mut dma_tx: DmaTxBuf = unsafe { DmaTxBuf::new(&mut TX_DESCRIPTORS, psram_buf).unwrap() };
     unsafe {
-        TX_DESCRIPTORS[NUM_DMA_DESC-1].next = &mut TX_DESCRIPTORS[0];
-        TX_DESCRIPTORS[NUM_DMA_DESC-1].set_suc_eof(true);
+        TX_DESCRIPTORS[NUM_DMA_DESC - 1].next = &mut TX_DESCRIPTORS[0];
+        TX_DESCRIPTORS[NUM_DMA_DESC - 1].set_suc_eof(true);
     }
 
     info!("Transfering");
@@ -251,7 +530,9 @@ pub async fn drive_display(buf_box: Box<[u8; FRAME_BYTES]>) {
     info!("buf_box3_len {}", buf_box3.len());
     loop {
         Timer::after_millis(100).await;
-        unsafe {cache_writeback_addr(buf_box3.as_ptr() as u32, buf_box3.len() as u32); }
+        unsafe {
+            cache_writeback_addr(buf_box3.as_ptr() as u32, buf_box3.len() as u32);
+        }
     }
 
     loop {
@@ -299,7 +580,7 @@ pub async fn app(mut buf_box2: Box<[u8; FRAME_BYTES]>) {
     }
 
     let [yellow_lo, yellow_hi] = Rgb565::YELLOW.into_storage().to_le_bytes();
-    for y in 0..479  {
+    for y in 0..479 {
         for x in [0, 400, 799] {
             buf_box2[(y * LCD_H_RES as usize + x) * 2] = yellow_lo;
             buf_box2[(y * LCD_H_RES as usize + x) * 2 + 1] = yellow_hi;
@@ -321,7 +602,7 @@ pub async fn app(mut buf_box2: Box<[u8; FRAME_BYTES]>) {
         let idx = (y * LCD_H_RES as usize + x) * 2;
 
         buf_box2[idx] = red_lo;
-        buf_box2[idx+1] = red_hi;
+        buf_box2[idx + 1] = red_hi;
         if x >= 799 {
             dx = -1;
         };
@@ -346,4 +627,3 @@ pub async fn app(mut buf_box2: Box<[u8; FRAME_BYTES]>) {
         // }
     }
 }
-
